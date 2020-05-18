2f2bd226810590a9471718678cd01f6e07d46a5c
AvocadoCasualty <baibeebambi@gmail.com>
2020-05-18 14:33:06 -0700

    Morning notes complete!

---
+++ nodeTest.js
@@ -0,0 +1,13 @@
+const andrew = {
+    name: 'Andrew',
+    age: 27,
+    favorites: {
+        color: 'red',
+        number: 38,
+        person: 'Becca'
+    }
+}
+function sayMyName(name) {
+    console.log(`hello my name is ${andrew.name}`)
+}
+sayMyName(andrew)
---
+++ server/index.js
@@ -0,0 +1,17 @@
+const express = require('express')
+
+const app = express()
+const ctrl = require('./controller')
+const SERVER_PORT = 4000
+
+app.use(express.json())
+
+app.get('/api/andrew', (req, res) => {
+    console.log('I GOT A REQUEST AT /api/andrew')
+    res.status(200).send('Andrew')
+})
+
+app.get('/api/users', ctrl.getAllUsers)
+app.get('/api/users/:user_id', ctrl.getUserById)
+
+app.listen(SERVER_PORT, () => console.log(`Server running on port ${SERVER_PORT}`))
